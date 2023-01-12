c608bb829e6383855c3802dfba9d605608f4bc87
Brynfor Morris <brynfor.morris@gmail.com>
2023-01-12 11:11:24 -0600

    commit tree 31fc70c820e39d441c68af11624be8bb1180bac2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const Array2 = [];
+
+function writeCards(names,event){
+    for(let i=0; i < names.length; i++){
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        Array2[i] = message;
+    }
+    return Array2;
+}
+
+function countDown(positiveInteger){
+    let downinc = positiveInteger;
+    while (downinc >= 0){
+        console.log(downinc--)
+    };
+}
